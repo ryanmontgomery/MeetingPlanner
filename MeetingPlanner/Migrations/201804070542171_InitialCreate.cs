7daf54820ee350cf4dbaafd079a3e56dd10120b0
Ryan Montgomery <mon16032@byui.edu>
2018-04-11 09:19:03 -0600

    A couple minor changes

--- MeetingPlanner/Migrations/201804070542171_InitialCreate.cs
+++ MeetingPlanner/Migrations/201804070542171_InitialCreate.cs
@@ -1,6 +1,5 @@
 namespace MeetingPlanner.Migrations
 {
-    using System;
     using System.Data.Entity.Migrations;
     
     public partial class InitialCreate : DbMigration
--- MeetingPlanner/Migrations/Configuration.cs
+++ MeetingPlanner/Migrations/Configuration.cs
@@ -1,9 +1,6 @@
 namespace MeetingPlanner.Migrations
 {
-    using System;
-    using System.Data.Entity;
     using System.Data.Entity.Migrations;
-    using System.Linq;
 
     internal sealed class Configuration : DbMigrationsConfiguration<MeetingPlanner.Models.MeetingPlannerContext>
     {
